ddbe54729f9bf817eddf27d56b8a327573957eae
Yuci Gou <yuci.gou@gmail.com>
2019-04-03 16:04:07 +0100

    Make menu stateful

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,36 +1,42 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
+class Menu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {topNavClass: 'topnav'}
   }
-}
 
-const Menu = () => {
-  return (
-    <div className="topnav" id="myTopnav">
-      <div className="dropdown">
-        <button className="dropbtn">Remedies&nbsp;
-          <i className="fa fa-caret-down"></i>
-        </button>
-        <div className="dropdown-content">
-          <Link to="/acupuncture">Acupuncture</Link>
-          <a href="#">Chinese Herbs</a>
-          <Link to="/cupping">Cupping</Link>
+  /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+  menuClickHanlder = () => {
+    if (this.state.topNavClass === "topnav") {
+      this.setState({topNavClass: "topnav responsive"})
+    } else {
+      this.setState({topNavClass: "topnav"})
+    }
+  }
+
+  render() {
+    return (
+      <div className={ this.state.topNavClass }>
+        <div className="dropdown">
+          <button className="dropbtn">Remedies&nbsp;
+            <i className="fa fa-caret-down"></i>
+          </button>
+          <div className="dropdown-content">
+            <Link to="/acupuncture" onClick={this.menuClickHanlder}>Acupuncture</Link>
+            <a href="#">Chinese Herbs</a>
+            <Link to="/cupping" onClick={this.menuClickHanlder}>Cupping</Link>
+          </div>
         </div>
+        <a href="#news">Testimonials</a>
+        <a href="#contact">Opening Hours</a>
+        <a href="#about" className="active">About Our Doctor</a>
+        <a href="#about">Contact Us</a>
+        <a href="javascript:void(0);" className="icon" onClick={this.menuClickHanlder}>&#9776;</a>
       </div>
-      <a href="#news">Testimonials</a>
-      <a href="#contact">Opening Hours</a>
-      <a href="#about" className="active">About Our Doctor</a>
-      <a href="#about">Contact Us</a>
-      <a href="javascript:void(0);" className="icon" onClick={myFunction}>&#9776;</a>
-    </div>
-  )
+    )
+  }
 }
 
 export default Menu
