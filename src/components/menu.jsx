6cd270a988ffc25eb96adac1dc720f0e47366651
Yuci Gou <yuci.gou@gmail.com>
2019-04-03 16:06:49 +0100

    Refactor method names

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -8,7 +8,7 @@ class Menu extends React.Component {
   }
 
   /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-  menuClickHanlder = () => {
+  menuToggler = () => {
     if (this.state.topNavClass === "topnav") {
       this.setState({topNavClass: "topnav responsive"})
     } else {
@@ -24,16 +24,16 @@ class Menu extends React.Component {
             <i className="fa fa-caret-down"></i>
           </button>
           <div className="dropdown-content">
-            <Link to="/acupuncture" onClick={this.menuClickHanlder}>Acupuncture</Link>
+            <Link to="/acupuncture" onClick={this.menuToggler}>Acupuncture</Link>
             <a href="#">Chinese Herbs</a>
-            <Link to="/cupping" onClick={this.menuClickHanlder}>Cupping</Link>
+            <Link to="/cupping" onClick={this.menuToggler}>Cupping</Link>
           </div>
         </div>
         <a href="#news">Testimonials</a>
         <a href="#contact">Opening Hours</a>
         <a href="#about" className="active">About Our Doctor</a>
         <a href="#about">Contact Us</a>
-        <a href="javascript:void(0);" className="icon" onClick={this.menuClickHanlder}>&#9776;</a>
+        <a href="javascript:void(0);" className="icon" onClick={this.menuToggler}>&#9776;</a>
       </div>
     )
   }
