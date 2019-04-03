f29c3fda780f265ac78484db33c62bc01c8e797c
Yuci Gou <yuci.gou@gmail.com>
2019-04-03 16:46:23 +0100

    Check which menu should be active by using React Router info

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, withRouter } from "react-router-dom";
 
 class Menu extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {topNavClass: 'topnav'}
+    this.state = {
+      topNavClass: 'topnav',
+      activeMenu: ''
+    }
   }
 
   /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
@@ -24,14 +27,26 @@ class Menu extends React.Component {
             <i className="fa fa-caret-down"></i>
           </button>
           <div className="dropdown-content">
-            <Link to="/acupuncture" onClick={this.menuToggler}>Acupuncture</Link>
+            <Link
+              to="/acupuncture"
+              onClick={this.menuToggler}
+              className = {this.props.location.pathname === '/acupuncture' ? 'active' : ''}
+            >
+              Acupuncture
+            </Link>
             <a href="#">Chinese Herbs</a>
-            <Link to="/cupping" onClick={this.menuToggler}>Cupping</Link>
+            <Link
+              to="/cupping"
+              onClick={this.menuToggler}
+              className = {this.props.location.pathname === '/cupping' ? 'active' : ''}
+            >
+              Cupping
+            </Link>
           </div>
         </div>
         <a href="#news">Testimonials</a>
         <a href="#contact">Opening Hours</a>
-        <a href="#about" className="active">About Our Doctor</a>
+        <a href="#about">About Our Doctor</a>
         <a href="#about">Contact Us</a>
         <a href="javascript:void(0);" className="icon" onClick={this.menuToggler}>&#9776;</a>
       </div>
@@ -39,4 +54,4 @@ class Menu extends React.Component {
   }
 }
 
-export default Menu
+export default withRouter(Menu)
