92b778e5de204fd4cecf0950246276e734446e7e
Yuci Gou <yuci.gou@gmail.com>
2020-02-28 10:28:16 +0000

    Enable styled component plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,13 @@ module.exports = {
     description: `Cambridge Chiese Acupuncture & Herbs is member of the Association of Traditional Chinese Medicine in the UK. Dr Li ZHANG has many years of clinical experiences both in China and the UK. You can contact us on +44(0)1223-412216, or visit us at 238 Mill Road, Cambridge, CB1 3NF, UK`,
     author: `Yuci Gou`
   },
-  plugins: [`gatsby-plugin-react-helmet`]
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      }
+    }
+  ]
 };
